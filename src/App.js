6b1c715252ed5d8436dffb067fd07e163d4b0033
Indykatz <cats83@me.com>
2022-07-12 23:54:32 +0100

    so far 2

--- src/App.js
+++ src/App.js
@@ -10,22 +10,38 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Photos from "./pages/Photos";
 
-const App = () => {
+const App = ({ setter }) => {
   const [user, setUser] = useState([]);
 
   return (
+    // all is flexed
     <TheApp>
+      {/* is just a header bannner - stlyed compont*/}
       <HeaderBanner />
+      {/* this is what routes this is where it begins - not a styled this is DOM  */}
       <BrowserRouter>
+        {/* this is my nav bar not import in dom but will be needed for your dom - styled not div*/}
         <Navbar />
+        {/* this is my styled component Content that is flex so responsive - not dom */}
         <Content>
+          {/* DOM - routeS - as it says  */}
           <Routes>
+            {/* ROUTE path */}
             <Route path="/" element={<Home user={user} setUser={setUser} />} />
-            <Route path="/Photos" element={<Photos user={user} />} />
+            {/* next path  */}
+            <Route
+              path="/Photos"
+              element={<Photos user={user} setter={setter} />}
+            />
+            {/* ends her */}
           </Routes>
+          {/* flex ends here */}
         </Content>
+        {/* footer */}
         <FooterBanner />
+        {/* endes router */}
       </BrowserRouter>
+      {/* ends app */}
     </TheApp>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,3 +17,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// <div class=NavbarContainer>
